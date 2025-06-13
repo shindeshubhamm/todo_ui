e645f148025291c1e25859e377fd3d55cb04d3a8
shindeshubhamm <shubhamshinde.3151@gmail.com>
2025-06-13 18:25:25 +0530

    clean project and chakra ui setup

--- app/lib/apollo-client.ts
+++ app/lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:3000/graphql",
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:1339/graphql",
 });
 
 export const client = new ApolloClient({
